3c0a52b42a8508a447d56c9c1f0fa6b6436891da
Souza <38145379+misael28@users.noreply.github.com>
2021-05-29 20:53:19 -0300

    feat: add container at screen to wrap form

--- lib/form_view.dart
+++ lib/form_view.dart
@@ -12,52 +12,60 @@ class _FormViewState extends State<FormView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login information"),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              validator: (text){
-                if(text == null || text.isEmpty){
-                  return 'Texto é obrigatorio';
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                hintText: 'Nome',
-              ),
-              onChanged: (text){
-                name = text;
-              },
-            ),
-            TextFormField(
-              validator:  (text){
-                if(text == null || text.isEmpty){
-                  return 'Sobrenome é obrigatorio';
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                hintText: "Sobrenome"
-              ),
-              onChanged: (text){
-                surName = text;
-              },
-            ),
-            ElevatedButton(onPressed: (){
-              final isValid = _formKey.currentState!.validate();
-              if(isValid){
-                showDialog(context: context, builder: (context) {
-                  return AlertDialog(
-                    title: Text('$name $surName'),
-                  );
-                });
-              }
-            }, child: Text("Validate"))
-          ],),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          width: MediaQuery.of(context).size.width * 0.7,
+          height: MediaQuery.of(context).size.height * 0.5,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (text){
+                    if(text == null || text.isEmpty){
+                      return 'Campo nome é obrigatorio';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Nome',
+                  ),
+                  onChanged: (text){
+                    name = text;
+                  },
+                ),
+                TextFormField(
+                  validator:  (text){
+                    if(text == null || text.isEmpty){
+                      return 'Campo sobrenome é obrigatorio';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Sobrenome"
+                  ),
+                  onChanged: (text){
+                    surName = text;
+                  },
+                ),
+                ElevatedButton(onPressed: (){
+                  final isValid = _formKey.currentState!.validate();
+                  if(isValid){
+                    showDialog(context: context, builder: (context) {
+                      return AlertDialog(
+                        title: Text('Bem vindo! $name $surName'),
+                      );
+                    });
+                  }
+                }, child: Text("Validar"))
+              ],),
+          ),
+        ),
       ),
     );
   }
