aa25438694e6c6f99878e9798e3e485ea026d871
Souza <38145379+misael28@users.noreply.github.com>
2021-05-29 21:14:07 -0300

    fix: correct layout at login form

--- lib/form_view.dart
+++ lib/form_view.dart
@@ -17,7 +17,7 @@ class _FormViewState extends State<FormView> {
         child: Container(
           decoration: BoxDecoration(
             color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: BorderRadius.circular(30),
           ),
           width: MediaQuery.of(context).size.width * 0.7,
           height: MediaQuery.of(context).size.height * 0.5,
@@ -25,44 +25,55 @@ class _FormViewState extends State<FormView> {
             key: _formKey,
             child: Column(
               children: [
-                TextFormField(
-                  validator: (text){
-                    if(text == null || text.isEmpty){
-                      return 'Campo nome é obrigatorio';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                    hintText: 'Nome',
+                Container(
+                  margin: EdgeInsets.all(20),
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: TextFormField(
+                    validator: (text){
+                      if(text == null || text.isEmpty){
+                        return 'Campo nome é obrigatorio';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Nome',
+                    ),
+                    onChanged: (text){
+                      name = text;
+                    },
                   ),
-                  onChanged: (text){
-                    name = text;
-                  },
                 ),
-                TextFormField(
-                  validator:  (text){
-                    if(text == null || text.isEmpty){
-                      return 'Campo sobrenome é obrigatorio';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                    hintText: "Sobrenome"
+                Container(
+                   margin: EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: TextFormField(
+                    validator:  (text){
+                      if(text == null || text.isEmpty){
+                        return 'Campo sobrenome é obrigatorio';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Sobrenome"
+                    ),
+                    onChanged: (text){
+                      surName = text;
+                    },
                   ),
-                  onChanged: (text){
-                    surName = text;
-                  },
                 ),
-                ElevatedButton(onPressed: (){
-                  final isValid = _formKey.currentState!.validate();
-                  if(isValid){
-                    showDialog(context: context, builder: (context) {
-                      return AlertDialog(
-                        title: Text('Bem vindo! $name $surName'),
-                      );
-                    });
-                  }
-                }, child: Text("Validar"))
+                Container(
+                   margin: EdgeInsets.all(15),
+                  child: ElevatedButton(onPressed: (){
+                    final isValid = _formKey.currentState!.validate();
+                    if(isValid){
+                      showDialog(context: context, builder: (context) {
+                        return AlertDialog(
+                          title: Text('Bem vindo! $name $surName'),
+                        );
+                      });
+                    }
+                  }, child: Text("Validar")),
+                )
               ],),
           ),
         ),
